63b902483ebb6eef141728e98e4163a72dae3737
devwulff <p.wulff887@gmail.com>
2022-06-20 14:17:26 +0200

    first merge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
-import MovieList from './components/MovieList';
+import "./App.css";
+import MovieList from "./components/MovieList";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Navigation />
       <MovieList />
+      <Footer />
     </div>
   );
 }
