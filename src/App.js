a1e1f24fd1437edb870c4e835bb54026384db5e4
devwulff <p.wulff887@gmail.com>
2022-06-21 10:35:38 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
@@ -6,16 +7,20 @@ import Navigation from "./components/Navigation";
 import MovieList from "./components/MovieList";
 
 function App() {
+  const [change, setChange] = useState("");
+  const handleOnChange = (elt) => {
+    setChange(elt.target.value);
+  };
+
   return (
     <div className="App">
-      <Navigation />
-      <MovieList />
+      <Navigation onChange={handleOnChange} />
+      <MovieList change={change} />
       {/* <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/movielist" element={<MovieList />} />
-        <Route path="/gallery/:id" element={<Details />} />
-      </Routes> */}
+        {/* <Route path="/movielist" element={<MovieList />} /> */}
+      {/* <Route path="/details/:id" element={<MovieList />} /> */}
+      {/* </Routes> */}
       <Footer />
     </div>
   );
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,25 +1,30 @@
 import { useState, useEffect } from "react";
 import MovieItem from "./MovieItem";
 
-const MovieList = () => {
+const MovieList = (props) => {
   const [details, setDetails] = useState([]);
+  let link;
 
   useEffect(() => {
-    fetch(
-      `https://api.themoviedb.org/4/list/8207676?page=1&api_key=f609f173487177fd0f9d5d8e5193e2fe&language=de-DE&append_to_response=videos,images`
-    )
+    if (props.change) {
+      link = `https://api.themoviedb.org/3/search/movie?&query=${props.change}&api_key=f609f173487177fd0f9d5d8e5193e2fe&language=de-DE&page=1&include_adult=false`;
+    } else {
+      link = `https://api.themoviedb.org/4/list/8207676?page=1&api_key=f609f173487177fd0f9d5d8e5193e2fe&language=de-DE&append_to_response=videos,images`;
+    }
+    fetch(link)
       .then((response) => response.json())
       .then((data) => {
         setDetails(data.results);
       });
-  }, []);
+  }, [props.change]);
 
   return (
     <div>
       {details.map((elt, i) => {
         return (
           <MovieItem
-            key={i}
+            key={elt.id}
+            id={elt.id}
             title={elt.title}
             year={elt.release_date}
             img={elt.poster_path}
