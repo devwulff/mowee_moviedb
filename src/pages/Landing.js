61b902b3a34414d124696043af49269b8953339b
devwulff <p.wulff887@gmail.com>
2022-06-23 13:58:51 +0200

    nearly finished v4

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,7 +4,7 @@ import ReactPlayer from "react-player";
 
 const Landing = () => {
   window.setTimeout(function () {
-    window.location.href = "./Home.js";
+    window.location.href = "/home";
   }, 5700);
 
   return (
